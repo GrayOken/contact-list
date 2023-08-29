4951b5027a87ceb6ff132656777ad63f7dd1c9d3
GrayOken <grayson.okenoas@gmail.com>
2023-08-29 10:56:20 -0700

    moving file

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -11,13 +11,13 @@ function FetchData() {
                 const response = await fetch("https://jsonplaceholder.typicode.com/users");
                 const data = await response.json();
                 console.log(data);
-                
+
                 setUsers(data);
             } catch (error) {
                 console.log("error", error);
             }
         }
-        
+
         grabAll();
     }, []);
     return (
